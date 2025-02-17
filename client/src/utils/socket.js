9f654cb0f20e71f3f94be63b29484d21b9bbac88
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2025-02-17 21:56:19 +0530

    update backend api

--- client/src/utils/socket.js
+++ client/src/utils/socket.js
@@ -1,7 +1,7 @@
 export default function socket(roomID, username) {
   //   const ws = new WebSocket(`ws://localhost:8080/ws/code/${roomID}`);
   const ws = new WebSocket(
-    `ws://https://codemeet-zzlo.onrender.com/ws/code/${roomID}`
+    `wss://codemeet-zzlo.onrender.com/ws/code/${roomID}`
   );
 
   ws.onopen = () => {
