04a02b746d605f3330bdf8fa95270409e56dbea2
Eshaan Pandey <eshaanpandey.ep@gmail.com>
2025-02-17 22:01:33 +0530

    add cors

--- server/main.go
+++ server/main.go
@@ -27,7 +27,7 @@ func main() {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:5173", 
+		AllowOrigins: "http://localhost:5173, https://codesmeet.netlify.app", 
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 	}))
